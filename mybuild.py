d29801b61f3a5e3d624708e9d68c3a1dc66c8391
memsharded <james@conan.io>
2022-12-01 13:00:16 +0100

    my custom list of packages

---
+++ mybuild.py
@@ -0,0 +1,37 @@
+import os
+
+
+def run(cmd):
+    ret = os.system(cmd)
+    if ret != 0:
+        raise Exception(f"Failed cmd: {cmd}")
+
+
+def load(f):
+    return open(f, "r").read()
+
+
+def export_pkgs(pkgs):
+    for pkg in pkgs:
+        name, version = pkg.split("/")
+        run(f"conan export recipes/{name}/all {version}@")
+
+def build_pkgs(pkgs):
+    profiles = os.listdir("myprofiles")
+    for pkg in pkgs:
+        name, version = pkg.split("/")
+        for profile in profiles:
+            run(f"conan install {name}/{version}@ -pr=myprofiles/{profile} --build=missing")
+
+
+run("conan config install https://github.com/conan-io/hooks.git -sf hooks -tf hooks")
+run("conan config set hooks.conan-center")
+
+
+
+pkgs = load("mypkgs.list").splitlines()
+export_pkgs(pkgs)
+build_pkgs(pkgs)
+run("conan search *")
+run(f"conan upload * -r={os.getenv('MYREMOTE')} --all -c")
+run("conan remove * -f")
---
+++ myprofiles/release.profile
@@ -0,0 +1,4 @@
+include(default)
+
+[options]
+zlib*:shared=True
